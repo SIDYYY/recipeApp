d868a61fec86e6733a755d08b9565443ae571c12
SIDYYY <carldaguinotas@gmail.com>
2025-01-21 16:00:58 +0800

    Redeployed + Clean UI

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -5,34 +5,38 @@ import { recipes } from '../../components/datas/recipe';
 
 export default function HomeScreen() {
   const router = useRouter();
-
+  const columns = 2;
   return (
-    <View className="flex-1 items-center bg-orange-200">
-      <View className="p-4">
-      <Text className="text-center text-lime-800 font-bold text-lg">
+    <View className="flex-1 items-center bg-white">
+    <View>
+      
+      <View className="pt-5 p-1 mb-4">
+      <Text className="text-center text-lime-800 font-bold text-xs">
           Created by Carl Patrick Daguinotas
         </Text>
-        <Text className="text-center text-lime-700 font-medium">
+        <Text className="text-center text-xs text-lime-700 font-medium">
           BSIT 3R10 - IT323
         </Text>
-        <Text className="text-3xl text-center font-extrabold text-lime-800 mb-4">RECIPE LIST</Text>
+        <Text className="text-3xl text-center font-extrabold text-green-600 mt-4 ">RECIPE LIST</Text>
       </View>
 
       <FlatList
         data={recipes}
+        numColumns={columns}
         renderItem={({ item }) => (
           <TouchableOpacity
-            className="shadow-sm rounded-lg p-4 mb-4 w-96 flex-row bg-lime-600"
+            className="mt-4  items-center rounded-lg p-4 mb-4 w-44 mx-3 border-2 border-gray-700 "
             onPress={() => router.push(`/nextPage?recipeId=${item.id}`)} 
           >
             <Image
               source={{ uri: item.imageUrl }}
-              style={{ width: '50%', height: 100, borderRadius: 8, marginRight: 16, aspectRatio: 1 }}
+              style={{ width: '100%', height: 100, borderRadius: 8 }}
             />
             <View className="flex-1 flex-col">
-              <Text className="text-2xl font-extrabold text-white">{item.title}</Text>
-              <Text className="text-white text-lg">{item.description}</Text>
+              <Text className="mt-2 pb-5 text-center text-lg font-extrabold text-green-800">{item.title}</Text>
             </View>
+            <Text className="text-center text-base">{item.calories} Calories</Text>
+
           </TouchableOpacity>
         )}
         keyExtractor={(item) => item.id}
@@ -41,6 +45,7 @@ export default function HomeScreen() {
       />
 
       
+    </View>
     </View>
   );
 }
--- app/nextPage.jsx
+++ app/nextPage.jsx
@@ -5,7 +5,6 @@ import { recipes } from '../components/datas/recipe';
 
 export default function NextPage() {
   const { recipeId } = useLocalSearchParams(); // Extracting recipeId from local params
-  const router = useRouter(); // The router for navigation
 
 
   const selectedRecipe = recipes.find((recipe) => recipe.id === recipeId);
@@ -13,41 +12,42 @@ export default function NextPage() {
   if (!selectedRecipe) {
     return (
       <View className="flex-1 justify-center items-center bg-gray-100 p-4">
-        <Text className="text-xl text-gray-600">Recipe not found or deleted.</Text>
+        <Text className="text-xl text-red-600">Recipe not found or deleted.</Text>
       </View>
     );
   }
 
   return (
-    <ScrollView className="flex-1 bg-orange-200 p-6">
+    <ScrollView className="flex-1 bg-white p-6 ">
 
-      <View className="bg-lime-600 rounded-2xl rounded-t-3xl ">
+      <View className="rounded-3xl rounded-t-3xl mb-12 border-2 border-black">
         
       {/* Recipe Image Section */}
       <Image
         source={{ uri: selectedRecipe.imageUrl }}
-        style={{ width: '100%', height: 250, borderRadius: 12, marginBottom: 20, borderTopRightRadius: 22, borderTopLeftRadius: 22 }}
+        style={{ width: '100%', height: 250, borderRadius: 12, marginBottom: 20, borderTopRightRadius: 18, borderTopLeftRadius: 18, borderBottomLeftRadius: 0, borderBottomRightRadius: 0 }}
       />
-      <Text className="text-white text-3xl font-extrabold  mb-4 px-5">{selectedRecipe.title}</Text>
+      <Text className="text-green-600 text-3xl font-extrabold  mb-4 px-5">{selectedRecipe.title}</Text>
+      <Text className="text-green-600 text-lg mb-7 px-5 ">{selectedRecipe.description}</Text>
 
       {/* Ingredients Section */}
-      <View className="mb-6 px-5">
-        <Text className="text-2xl font-semibold text-white mb-2 ">Ingredients:</Text>
-        <View className="flex-col space-y-2 px-5">
+      <View className="mb-6 px-6">
+        <Text className="text-2xl font-semibold text-green-600 mb-2 ">Ingredients:</Text>
+        <View className="flex-col space-y-2 px-7 ">
           {selectedRecipe.ingredients.split(',').map((ingredient, index) => (
-            <Text key={index} className="text-lg text-white">{ingredient.trim()}</Text>
+            <Text key={index} className="text-lg text-gray-800">{ingredient.trim()}</Text>
           ))}
         </View>
       </View>
 
       {/* Instructions Section */}
       <View className="pb-10">
-        <Text className="text-2xl font-semibold text-white mb-2 px-5">Instructions:</Text>
-        <View className="flex-col space-y-2 px-5">
+        <Text className="text-2xl font-semibold text-green-600 mb-2 px-6">Instructions:</Text>
+        <View className="flex-col space-y-2 px-7">
           {/* Split the instructions by each step */}
           {selectedRecipe.instructions.split('.').map((step, index) => (
             step.trim() && (
-              <Text key={index} className="text-lg text-white px-5 ">{step.trim()}.</Text>
+              <Text key={index} className="text-lg text-gray-800 px-5 ">{step.trim()}.</Text>
             )
           ))}
         </View>
